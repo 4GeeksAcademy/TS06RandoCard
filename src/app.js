2f29bc8062f8895defd32ecde1be8aa7a016517f
Tim <141640156+TStokes06@users.noreply.github.com>
2023-10-14 16:27:15 +0000

    TS06RandoCard

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "bootstrap";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+
+  document.getElementById("newCard").addEventListener("click", function() {
+    generateCard();
+  });
 };
+
+function generateCard() {
+  const suits = ["heart", "diamond", "spade", "club"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const card = document.getElementById("card");
+
+  // Remove existing classes
+  card.className = "";
+
+  // Add new classes
+  card.classList.add("card", randomSuit);
+
+  // Update card content
+  card.querySelector(".top").textContent = getSuitSymbol(randomSuit);
+  card.querySelector(".middle").textContent = randomValue;
+  card.querySelector(".bottom").textContent = getSuitSymbol(randomSuit);
+}
+
+function getSuitSymbol(suit) {
+  switch (suit) {
+    case "heart":
+      return "\u2665";
+    case "diamond":
+      return "\u2666";
+    case "spade":
+      return "\u2660";
+    case "club":
+      return "\u2663";
+    default:
+      return "";
+  }
+}
