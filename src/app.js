970948079de4988a65a8f8934f7662849fa623bd
Tim <141640156+TStokes06@users.noreply.github.com>
2023-10-18 20:20:33 +0000

    ECadd

--- src/app.js
+++ src/app.js
@@ -7,7 +7,47 @@ window.onload = function() {
   document.getElementById("newCard").addEventListener("click", function() {
     generateCard();
   });
+
+  document.getElementById("cardWidth").addEventListener("change", function() {
+    document.getElementById("card").style.width = this.value + "px";
+  });
+
+  document.getElementById("cardHeight").addEventListener("change", function() {
+    document.getElementById("card").style.height = this.value + "px";
+  });
 };
+
+document.getElementById("newCard").addEventListener("click", generateNewCard);
+
+document.getElementById("cardWidth").addEventListener("input", updateCardSize);
+document.getElementById("cardHeight").addEventListener("input", updateCardSize);
+
+function generateNewCard() {
+  // Generate a new card...
+}
+
+// Set a timer to generate a new card after 10 seconds
+setTimeout(generateNewCard, 10000);
+
+function updateCardSize() {
+  const cardWidth = document.getElementById("cardWidth").value;
+  const cardHeight = document.getElementById("cardHeight").value;
+  const error = document.getElementById("error");
+
+  if (
+    cardWidth < 150 ||
+    cardWidth > 400 ||
+    cardHeight < 150 ||
+    cardHeight > 400
+  ) {
+    error.textContent = "Card width and height must be between 150 and 400.";
+  } else {
+    error.textContent = "";
+    const card = document.getElementById("card");
+    card.style.width = `${cardWidth}px`;
+    card.style.height = `${cardHeight}px`;
+  }
+}
 
 function generateCard() {
   const suits = ["heart", "diamond", "spade", "club"];
@@ -58,3 +98,29 @@ function getSuitSymbol(suit) {
       return "";
   }
 }
+let cardInterval;
+
+window.onload = function() {
+  generateCard();
+
+  // Start the card generation interval
+  cardInterval = setInterval(generateCard, 10000);
+
+  document.getElementById("newCard").addEventListener("click", function() {
+    // Stop the current interval
+    clearInterval(cardInterval);
+
+    generateCard();
+
+    // Start a new interval
+    cardInterval = setInterval(generateCard, 10000);
+  });
+
+  document.getElementById("cardWidth").addEventListener("change", function() {
+    document.getElementById("card").style.width = this.value + "px";
+  });
+
+  document.getElementById("cardHeight").addEventListener("change", function() {
+    document.getElementById("card").style.height = this.value + "px";
+  });
+};
